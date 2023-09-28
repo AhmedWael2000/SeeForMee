2fb6b50d4881315f554b4d1fcda6c511f8472f7e
Aya Ashraf <ayaa40722@gmail.com>
2023-09-28 13:11:07 +0300

    Update preprocessing.py preprocessing the image

--- OCR/preprocessing.py
+++ OCR/preprocessing.py
@@ -1,3 +1,13 @@
+
+import cv2
+
+
+
 def preprocessing(img):
-   
-    return img
+    img_ = cv2.imread(img)  # to make the image be binary color (black&white)
+    img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+    img_ = cv2.resize(img_, (900, 950))
+    img_result = cv2.adaptiveThreshold(img_, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,  # use adaptive_Threshold library
+                                            cv2.THRESH_BINARY, 17, 5)
+
+    return img_result

8bff3ef9dea944a75aa61a173d4c3ffa6096bc27
Aya Ashraf <ayaa40722@gmail.com>
2023-09-28 13:13:03 +0300

    Update OCR.py apply ocr in image after the preprocessing

--- OCR/OCR.py
+++ OCR/OCR.py
@@ -1,3 +1,15 @@
+import pytesseract
+from pytesseract import Output
+
+
+myconfig = r"--psm 11 --oem 3"
+
 def OCR(img):
-    text = None
+    data = pytesseract.image_to_data(img, config=myconfig, output_type=Output.DICT)
+    amount_box = len(data['text'])
+    text = ""
+    for i in range(amount_box):
+        if float(data['conf'][i]) > 80:
+            text += data['text'][i] + " "
     return text
+
